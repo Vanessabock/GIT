d68e123c15791b9a9805f73c199afaf1f847a021
PaulKreft <64103063+PaulKreft@users.noreply.github.com>
2023-11-29 11:36:47 +0100

    force conflict

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hallo Git von Paul");
+        System.out.println("Hallo Git");
     }
     public static boolean checkPalindrome(String value){
         return new StringBuilder(value).reverse().toString().equals(value);
