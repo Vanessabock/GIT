9dcec87571f55cda99dd7bd37a9800b2802e626c
vanessabock <vanessabock54@gmail.com>
2023-11-29 11:38:42 +0100

    Use of checkPalindrome

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hallo Git von Paul");
+        System.out.println(checkPalindrome("ABBA"));
     }
     public static boolean checkPalindrome(String value){
         return new StringBuilder(value).reverse().toString().equals(value);
