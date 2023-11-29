85f93066dc2451f96ff829559f93bdb3c2cef102
Sviat <slsvyatko@gmail.com>
2023-11-29 11:26:15 +0100

    added checkPalindrome

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hallo Git von Paul");
     }
+    public static boolean checkPalindrome(String value){
+        return new StringBuilder(value).reverse().toString().equals(value);
+    }
 }
